3915c4cf9eb88a0176006d5925993d4956aff05e
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-22 09:17:25 +0300

    Add files via upload

---
+++ lab7(1).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	int a[12][10], k = 1;
+	for (int j = 9; j >= 0; j--)
+	{
+		for (int i = 0; i <=11; i++, k++)
+		{
+			a[i][j] = k;
+			//cout << a[i][j] << setw(4);
+		}
+			//cout << endl;
+	}
+
+	for (int i = 0; i < 12; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << a[i][j] << setw(4);
+		}
+		cout << endl;
+	}
+}
