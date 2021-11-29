9d9bca6cb7c26b2533716663ca1dfbb4999e6aa3
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-29 19:10:44 +0200

    Update lab7(5).cpp

--- lab7(5).cpp
+++ lab7(5).cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <stack> 
 using namespace std;
 int main()
 {
-const int n=5;
-int a[n][n], i, j, m=0, v=0;
-for (j=0;j<n;j++)
- for (i=0;i<n;i++)
-  {a[i][j]= rand()%10;
-   if (a[i][j]%2) {int h[]=a[i][j]; m++;}
-    else {int k[]=a[i][j]; v++;}
-   }
-for (i=0;i<m;i++) cout<< h[i]<<" ";
-cout <<endl;
-for (i=0;i<v;i++) cout<< k[i]<<" ";
+    stack <int> steck1;
+    stack <int> steck2;
+    const int n = 5;
+    int a[n][n],h[n],k[n], i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            a[i][j] = rand() % 10;
+            if (a[i][j] % 2)
+                steck1.push(a[i][j]);
+            else
+                steck2.push(a[i][j]);
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (i = 0; i < n * n; i++, steck1.pop()) h[i] = steck1.top();
+    for (i = 0; i < n * n; i++, steck2.pop()) k[i] = steck2.top();
+    for (i = 0; i < n * n; i++)
+        if (h[i] != 0) cout << h[i] << " ";
+    cout << endl;
+    for (i = 0; i < n * n; i++)
+        if (k[i] != 0) cout << k[i] << " ";
+    cout << endl;
 }
